4034aec29ca317790da7422ba40e83833af1acfe
Steven Bui <bui.steven.d@gmail.com>
2022-11-16 10:43:26 -0800

    feat: Add update treat capability
    Added PUT handler to update treats table.
    Editing treat will fill in ADD fields automatically

--- src/views/treats/index.js
+++ src/views/treats/index.js
@@ -10,18 +10,26 @@ function Treats() {
 
     const [isAdding, setIsAdding] = useState(false);
     const [addParams, setAddParams] = useState({
+        id: null,
         name: '',
         desc: '',
         cals: 0,
-    })
+    });
     async function handleSubmit() {
-        let newTreat = {
-            ...addParams
-        };
-        await TreatsService.addTreat(newTreat);
+
+        // if there is an id, this is an update, not a create
+        if (addParams.id !== null) {
+            // here we just want to update
+            await TreatsService.updateTreat(addParams)
+        } else {
+            let newTreat = {
+                ...addParams
+            };
+            await TreatsService.addTreat(newTreat);   
+        }
         let updatedTreats = await TreatsService.getTreats();
         setTreats(updatedTreats);
-        setIsAdding(false)
+        setIsAdding(false);
     }
 
     async function removeTreat(treatId) {
@@ -30,33 +38,47 @@ function Treats() {
         setTreats(updatedTreats);
     }
 
+    function editTreat({id, name, description, calories}) {
+        // set add params
+        setAddParams({
+            ...addParams,
+            id,
+            name,
+            desc: description,
+            cals: calories,
+        });
+        setIsAdding(true);
+    }
+
     return(<>
         <p>treats view</p>
-        {!isAdding && (
+        {!isAdding ? (
             <>
                 <button onClick={() => setIsAdding(true)}>add treat</button>
                 <ul>
-                    {treats.map((treat, idx) => {
+                    {treats.map((treat) => {
                         return (
                             <li key={treat.id}>
                                 <p>{treat.name}</p>
                                 <p>{treat.description}</p>
                                 <p>cals: {treat.calories}</p>
-                                <button onClick={() => {
-                                    removeTreat(treat.id);
-                                }}>remove</button>
+                                <div>
+                                    <button onClick={() => {removeTreat(treat.id)}}>remove</button>
+                                    <button onClick={() => {editTreat(treat)}}>edit</button>
+                                </div>
                             </li>
                         )
                     })}
                 </ul>
             </>
-        ) || (
-        <>
-            <div>name: <input type="text" onChange={(e) => setAddParams({...addParams, name: e.target.value})}></input></div>
-            <div>desc: <input type="text" onChange={(e) => setAddParams({...addParams, desc: e.target.value})}></input></div>
-            <div>cals: <input type="number" onChange={(e) => setAddParams({...addParams, cals: e.target.value})}></input></div>
-            <button onClick={handleSubmit}>submit</button>
-        </>)}
+        ) : (
+            <>
+                <div>name: <input type="text" value={addParams.name || ''} onChange={(e) => setAddParams({...addParams, name: e.target.value})}></input></div>
+                <div>desc: <input type="text" value={addParams.desc || ''} onChange={(e) => setAddParams({...addParams, desc: e.target.value})}></input></div>
+                <div>cals: <input type="number" value={addParams.cals || ''}onChange={(e) => setAddParams({...addParams, cals: e.target.value})}></input></div>
+                <button onClick={handleSubmit}>submit</button>
+            </>
+        )}
     </>);
 }
 
