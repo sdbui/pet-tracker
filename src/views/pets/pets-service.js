d310b9be29fed370f6b49673d643bed39761c94c
Steven Bui <bui.steven.d@gmail.com>
2022-11-21 11:03:33 -0800

    feat: Add ability to feed a dog
    created endpoint that will insert into feedings table

--- src/views/pets/pets-service.js
+++ src/views/pets/pets-service.js
@@ -4,6 +4,18 @@ const PetsService = {
         let results = await fetch(url);
         let json = await results.json();
         return json.data;
+    },
+    feedPet: async ({petId, treatId, amount}) => {
+        let url = '/api/pets/feed';
+        let postBody = { petId, treatId, amount };
+        let results = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(postBody)
+        })
+
     }
 }
 
